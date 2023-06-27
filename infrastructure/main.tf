7be67859225dfff3d4cd78272774cfbed4c85dea
alanlviana <alanlviana@hotmail.com>
2023-06-27 16:32:13 -0300

    feat: Creating log group to keep logs from Lambda Notification execution.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -48,6 +48,17 @@ resource "aws_lambda_function" "lambda_notification_s3" {
   ]
 }
 
+resource "aws_cloudwatch_log_group" "lambda_notification_s3_log_group" {
+  name              = "/aws/lambda/${aws_lambda_function.lambda_notification_s3.function_name}"
+  retention_in_days = 7
+  lifecycle {
+    prevent_destroy = false
+  }
+  depends_on = [
+    aws_lambda_function.lambda_notification_s3_log_group
+  ]
+}
+
 resource "aws_lambda_permission" "allow_bucket" {
   statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
